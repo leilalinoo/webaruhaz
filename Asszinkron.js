cbd8c8a9fe3846af723733ef1e87aa6686d6cf25
leilalinoo <aleila0921@gmail.com>
2023-05-05 13:49:02 +0200

    MINDEN MUKODIK

--- Asszinkron.js
+++ Asszinkron.js
@@ -15,6 +15,12 @@ class Asszinkron{
     adatTorol(vegpont, id){
         //ezt az id-jú elemet kell törölni
         vegpont = vegpont+"/"+id
+        fetch(vegpont, {method: "delete", headers:{"Content-Type":"application.json"}})
+          .then((response) => response.json())
+          .then((data) => {
+            
+          })
+          .catch((error) => console.log(error));
 
         
     }
