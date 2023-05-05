5b3d56d8d65d98e72d9df3780c807d77dfa7ef0f
leilalinoo <aleila0921@gmail.com>
2023-05-05 12:25:55 +0200

    kedvenclista pusholasa

--- Termek.js
+++ Termek.js
@@ -1,19 +1,33 @@
 import { termekLista } from "./adatok.js";
-import { favList } from "./kedvencek.js";
+
 
 class Termek {
   constructor(szuloElem, elem) {
     this.szuloElem = szuloElem;
     this.elem = elem;
     szuloElem.append(
-      `<div><h2>${elem.iro}</h2><h3>${elem.cim}</h3><p>${elem.ar} Ft</p><button type="button">Kedvenc</button></div>`
+      `<div class="card"><div class="card-header">${elem.iro}</div><div class="card-body">${elem.cim}</div> <div class="card-footer">${elem.ar} Ft</div><button type="button">Kedvenc</button></div>`
     );
+    /*
+<div class="card">
+  <div class="card-header">Header</div>
+  <div class="card-body">Content</div>
+  <div class="card-footer">Footer</div>
+</div>
+    */
 
     const BTTN = $(`div:last-child button`);
 
     BTTN.on("click", () => {
-        favList.push(elem)
+      this.esemenyTrigger();
     });
+  }
+
+  esemenyTrigger() {
+    const esemeny = new CustomEvent("kedvenc", {
+      detail: this,
+    });
+    window.dispatchEvent(esemeny);
   }
 }
 
--- Termekek.js
+++ Termekek.js
@@ -1,12 +1,19 @@
 import Termek from "./Termek.js";
 import { termekLista } from "./adatok.js";
+
 class Termekek {
+  #kedvencek = [];
   constructor() {
     const szuloElem = $("article");
     for (let index = 0; index < termekLista.length; index++) {
-        const t1 = new Termek(szuloElem, termekLista[index]);
+      const t1 = new Termek(szuloElem, termekLista[index]);
     }
+
+    $(window).on("kedvenc", (event) => {
+      this.#kedvencek.push(event.detail);
+      console.log(this.#kedvencek)
+    });
   }
 }
 
-export default Termekek
+export default Termekek;
